3d3f46a6bcc98ce8fea4c1e3aaacbc423bbd6a0f
fatima <fatimahassanhassab8@gmail.com>
2024-07-25 10:02:48 -0700

    cru section done1

--- routes/sectionRoutes.js
+++ routes/sectionRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const { cloudinaryConfig, upload } = require('../cluodinaryMulter');
-const { addSection,getSections } = require("../controllers/sectionController")
+const { addSection,getSections ,updateSection} = require("../controllers/sectionController")
 
 const router = express.Router()
 
-router.post("/addSection", upload.single('image'),addSection)
-router.get("/getSections",getSections)
+router.post("/addSection", upload.single('image'),addSection);
+router.get("/getSections",getSections);
+router.put('/updateSection/:id',upload.single('image'),updateSection);
 // router.post("/signup", signupController)
 // router.put("/updateUser", updateUser)
 
