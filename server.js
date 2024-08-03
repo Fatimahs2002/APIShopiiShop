36939a46da146c17624c184ef0504819a590c234
adelFtouni <101456560+adelFtouni@users.noreply.github.com>
2024-08-03 03:40:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,10 +12,13 @@ const storeRoutes = require('./routes/storeRoutes');
 
 const { app, server, io } = require('./io');
 
-app.use(cors({
-  origin: 'https://shopiishop.web.app/',
-  credentials: true
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://shopiishop.web.app');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(cookieParser());
 
 
