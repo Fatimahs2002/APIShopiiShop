04d4b5eb1a317fb41506a579f87f2a6bb5a5cd3a
adelFtouni <101456560+adelFtouni@users.noreply.github.com>
2024-08-03 03:39:57 +0300

    Update io.js

--- io.js
+++ io.js
@@ -4,13 +4,17 @@ const { Server } = require('socket.io');
 const app = express();
 const cors = require('cors');
 const server = http.createServer(app);
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://shopiishop.web.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 const io = new Server(server, {
   cors: {
     origin: ['http://localhost:3000', 'https://shopiishop.web.app'],
-    methods: ['GET', 'POST', 'PUT','DELETE'],
-     allowedHeaders: ['Content-Type', 'Authorization']
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   }
 });
 
@@ -22,3 +26,4 @@ io.on('connection', (socket) => {
 });
 
 module.exports = { app, server, io };
+
